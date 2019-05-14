f0e3aadbc681b44ae4aba387ecc77771180635c3
Martin Jindra <jindra.martin@hotmail.com>
2019-05-14 11:42:16 +0200

    Neue Nachricht-Button ordentlich hinzugefügt

--- app/src/main/java/com/simplchat/simplchat/simplchat/MainActivity.java
+++ app/src/main/java/com/simplchat/simplchat/simplchat/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.TextView;
 import java.io.BufferedReader;
@@ -32,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
         pass = settings.getString("pass","");
     }
 
+    /**
+     * Wenn man in die App zurückkehrt, nur dann aktualisiert es den Chat
+     */
     @Override
     protected void onResume() {
         super.onResume();
@@ -39,10 +44,13 @@ public class MainActivity extends AppCompatActivity {
         this.timer.schedule(new Refresher(), 0, 1000);
     }
 
+    /**
+     * Zerstört die Aktualisierung, beim Verlassen der App um Recourssen zu sparen
+     */
     @Override
-    protected void onDestroy() {
+    protected void onPause() {
         timer.cancel();
-        super.onDestroy();
+        super.onPause();
     }
 
     /**
